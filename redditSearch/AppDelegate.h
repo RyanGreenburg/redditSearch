3dd6dfb9ad4c81e5e50ed8550e0adcdf8cf73422
Ryan Greenburg <ryangreenburg@mac.com>
2019-02-13 11:16:05 -0700

    Initial Commit

---
+++ redditSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  redditSearch
+//
+//  Created by RYAN GREENBURG on 2/13/19.
+//  Copyright © 2019 RYAN GREENBURG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
